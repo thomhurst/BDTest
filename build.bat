2466743d29f70a78acc4530247443c1fc497d4b6
Tom Longhurst <tom.longhurst@asos.com>
2019-01-24 09:43:18 +0000

    Dotnet Pack command working instead of Nuget Pack

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
-dotnet build "./BDTest/BDTest.csproj"
-dotnet build "./BDTest/BDTest.csproj" --configuration Release
-nuget pack "./BDTest/BDTest.csproj"
+dotnet build "C:\git\BDTest\BDTest\BDTest.csproj"
+dotnet build "C:\git\BDTest\BDTest\BDTest.csproj" --configuration Release
+dotnet pack "C:\git\BDTest\BDTest\BDTest.csproj" /p:NuspecFile="C:\git\BDTest\BDTest\BDTest.nuspec" --configuration Release --output "C:\git\BDTest" --no-build
 
-dotnet build "./BDTest.ReportGenerator/BDTest.ReportGenerator.csproj"
-dotnet build "./BDTest.ReportGenerator/BDTest.ReportGenerator.csproj" --configuration Release
-nuget pack "./BDTest.ReportGenerator/BDTest.ReportGenerator.nuspec"
+dotnet build "C:\git\BDTest\BDTest.ReportGenerator\BDTest.ReportGenerator.csproj"
+dotnet build "C:\git\BDTest\BDTest.ReportGenerator\BDTest.ReportGenerator.csproj" --configuration Release
+dotnet pack "C:\git\BDTest\BDTest.ReportGenerator\BDTest.ReportGenerator.csproj" /p:NuspecFile="C:\git\BDTest\BDTest.ReportGenerator\BDTest.ReportGenerator.nuspec" --configuration Release --output "C:\git\BDTest" --no-build
 
 pause
