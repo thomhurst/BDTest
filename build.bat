3387dbdb36d91d1068b52bc29c162f8c72c1085b
Tom Longhurst <tom.longhurst@asos.com>
2019-01-29 16:55:16 +0000

    Extracted NUnit to separate project and nuget package so that NUnit dependencies aren't forced on the user

--- build.bat
+++ build.bat
@@ -2,6 +2,10 @@ dotnet build "C:\git\BDTest\BDTest\BDTest.csproj"
 dotnet build "C:\git\BDTest\BDTest\BDTest.csproj" --configuration Release
 dotnet pack "C:\git\BDTest\BDTest\BDTest.csproj" /p:NuspecFile="C:\git\BDTest\BDTest\BDTest.nuspec" --configuration Release --output "C:\git\BDTest" --no-build
 
+dotnet build "C:\git\BDTest\BDTest.NUnit\BDTest.NUnit.csproj"
+dotnet build "C:\git\BDTest\BDTest.NUnit\BDTest.NUnit.csproj" --configuration Release
+dotnet pack "C:\git\BDTest\BDTest.NUnit\BDTest.NUnit.csproj" /p:NuspecFile="C:\git\BDTest\BDTest.NUnit\BDTest.NUnit.nuspec" --configuration Release --output "C:\git\BDTest" --no-build
+
 dotnet build "C:\git\BDTest\BDTest.ReportGenerator\BDTest.ReportGenerator.csproj"
 dotnet build "C:\git\BDTest\BDTest.ReportGenerator\BDTest.ReportGenerator.csproj" --configuration Release
 dotnet pack "C:\git\BDTest\BDTest.ReportGenerator\BDTest.ReportGenerator.csproj" /p:NuspecFile="C:\git\BDTest\BDTest.ReportGenerator\BDTest.ReportGenerator.nuspec" --configuration Release --output "C:\git\BDTest" --no-build
