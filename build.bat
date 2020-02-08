53f88d7f09b8f3e53ed7b1472d20f08088ec6bb1
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-02-08 17:01:04 +0000

    Fix Nuspec Files

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 set /p version="Enter Version Number to Build With: "
 
 @echo on
-dotnet pack ".\BDTest\BDTest.csproj"  --configuration Release /p:Version=%version% /p:NuspecFile="BDTest.nuspec"
+dotnet pack ".\BDTest\BDTest.csproj"  --configuration Release /p:Version=%version% /p:NuspecFile="BDTest.nuspec" 
 
 dotnet pack ".\BDTest.NUnit\BDTest.NUnit.csproj"  --configuration Release /p:Version=%version% /p:NuspecFile="BDTest.NUnit.nuspec"
 
