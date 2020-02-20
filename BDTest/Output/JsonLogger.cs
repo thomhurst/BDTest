0e93b1e94da8b41f8230423ed8907bb6d35acdb9
tom.longhurst <tom.longhurst@asos.com>
2020-02-20 14:13:05 +0000

    Fix write scenario with self referencing loops

--- BDTest/Output/JsonLogger.cs
+++ BDTest/Output/JsonLogger.cs
@@ -8,9 +8,14 @@ namespace BDTest.Output
 
     internal static class JsonLogger
     {
+        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        };
+        
         internal static void WriteScenario(Scenario scenario)
         {
-            File.WriteAllText(FileLocations.RandomScenarioFilePath, JsonConvert.SerializeObject(scenario, Formatting.Indented));
+            File.WriteAllText(FileLocations.RandomScenarioFilePath, JsonConvert.SerializeObject(scenario, Formatting.Indented, Settings));
         }
     }
 }
