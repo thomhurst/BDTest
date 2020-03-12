1593223a6be452df50494e0ff2698c34227a3b75
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-03-12 16:20:20 +0000

    StepTextHelper.cs

---
+++ BDTest/Helpers/StepTextHelper.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using BDTest.Attributes;
+using Humanizer;
+
+namespace BDTest.Helpers
+{
+    public static class StepTextHelper
+    {
+        public static string GetStepText(Expression<Action> action)
+        {
+            return GetStepText(action.Body as MethodCallExpression);
+        }
+        
+        public static string GetStepText(Expression<Func<Task>> action)
+        {
+            return GetStepText(action.Body as MethodCallExpression);
+        }
+        
+        private static string GetStepText(MethodCallExpression methodCallExpression)
+        {
+            var arguments = GetMethodArguments(methodCallExpression);
+
+            var methodInfo = methodCallExpression?.Method;
+
+            var stepTextAttribute = (StepTextAttribute)(methodInfo?.GetCustomAttributes(
+                                                            typeof(StepTextAttribute), true) ??
+                                                        new string[] { }).FirstOrDefault();
+            
+            var customStepText =
+                stepTextAttribute?.Text;
+            
+            var methodNameHumanized = methodInfo?.Name.Humanize();
+
+            if (customStepText != null)
+            {
+                try
+                {
+                    customStepText = UseStepTextParameterOverrides(stepTextAttribute);
+                    customStepText = string.Format(customStepText, arguments.Cast<object>().ToArray());
+                }
+                catch (Exception)
+                {
+                    throw new ArgumentException(
+                        $"Step Text arguments are wrong.\nTemplate is: {customStepText}\nArguments are {string.Join(",", arguments)}");
+                }
+            }
+
+            return customStepText ?? methodNameHumanized;
+        }
+        
+        private static List<string> GetMethodArguments(MethodCallExpression methodCallExpression)
+        {
+            var arguments = new List<string>();
+
+            if (methodCallExpression?.Arguments != null)
+            {
+                foreach (var argument in methodCallExpression.Arguments)
+                {
+                    var value = GetExpressionValue(argument);
+                    arguments.Add(value ?? "null");
+                }
+            }
+
+            return arguments;
+        }
+        
+        private static string GetExpressionValue(Expression argument)
+        {
+            try
+            {
+                var compiledExpression = Expression.Lambda(argument).Compile().DynamicInvoke();
+
+                if (compiledExpression == null)
+                {
+                    return "null";
+                }
+
+                if (TypeHelper.IsFuncOrAction(compiledExpression.GetType()))
+                {
+                    var func = (Delegate) compiledExpression;
+
+                    return func.DynamicInvoke()?.ToString();
+                }
+
+                if (TypeHelper.IsIEnumerable(compiledExpression) || compiledExpression.GetType().IsArray)
+                {
+                    return string.Join(", ", (IEnumerable<object>) compiledExpression);
+                }
+
+                if (TypeHelper.IsIDictionary(compiledExpression))
+                {
+                    return string.Join(",",
+                        ((IDictionary<object, object>) compiledExpression).Select(kv => $"{kv.Key}={kv.Value}")
+                    );
+                }
+
+                return compiledExpression.ToString();
+            }
+            catch (Exception)
+            {
+                return "null";
+            }
+        }
+
+        private static string UseStepTextParameterOverrides(StepTextAttribute stepTextAttribute)
+        {
+            return stepTextAttribute.StepTextParameterOverrides.Aggregate(stepTextAttribute.Text, (current, parameterOverride) => current.Replace($"{{{parameterOverride.Split(':')[0]}}}", parameterOverride.Split(new [] {':'}, 2)[1]));
+        }
+    }
+}
--- BDTest/Test/Step.cs
+++ BDTest/Test/Step.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
-using Humanizer;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using BDTest.Attributes;
 using BDTest.Exceptions;
 using BDTest.Helpers;
 using BDTest.Output;
 using BDTest.Test.Steps;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace BDTest.Test
 {
@@ -71,104 +66,10 @@ namespace BDTest.Test
                 StepText = $"{StepPrefix} {OverriddenStepText.Invoke()}";
                 return;
             }
-            
-            MethodCallExpression methodCallExpression;
-            if (Runnable.Action != null)
-            {
-                methodCallExpression = Runnable.Action.Body as MethodCallExpression;
-            }
-            else
-            {
-                methodCallExpression = Runnable.Task.Body as MethodCallExpression;
-            }
 
-            var arguments = GetMethodArguments(methodCallExpression);
+            var customStepText = Runnable.Action != null ? StepTextHelper.GetStepText(Runnable.Action) : StepTextHelper.GetStepText(Runnable.Task);
 
-            var methodInfo = methodCallExpression?.Method;
-
-            var stepTextAttribute = ((StepTextAttribute)(methodInfo?.GetCustomAttributes(
-                                                             typeof(StepTextAttribute), true) ??
-                                                         new string[] { }).FirstOrDefault());
-            
-            var customStepText =
-                stepTextAttribute?.Text;
-            
-            var methodNameHumanized = methodInfo?.Name.Humanize();
-
-            if (customStepText != null)
-            {
-                try
-                {
-                    customStepText = UseStepTextParameterOverrides(stepTextAttribute);
-                    customStepText = string.Format(customStepText, arguments.Cast<object>().ToArray());
-                }
-                catch (Exception)
-                {
-                    throw new ArgumentException(
-                        $"Step Text arguments are wrong.\nTemplate is: {customStepText}\nArguments are {string.Join(",", arguments)}");
-                }
-            }
-
-            StepText = $"{StepPrefix} {customStepText ?? methodNameHumanized}";
-        }
-
-        private string UseStepTextParameterOverrides(StepTextAttribute stepTextAttribute)
-        {
-            return stepTextAttribute.StepTextParameterOverrides.Aggregate(stepTextAttribute.Text, (current, parameterOverride) => current.Replace($"{{{parameterOverride.Split(':')[0]}}}", parameterOverride.Split(new [] {':'}, 2)[1]));
-        }
-
-        private static List<string> GetMethodArguments(MethodCallExpression methodCallExpression)
-        {
-            var arguments = new List<string>();
-
-            if (methodCallExpression?.Arguments != null)
-            {
-                foreach (var argument in methodCallExpression.Arguments)
-                {
-                    var value = GetExpressionValue(argument);
-                    arguments.Add(value ?? "null");
-                }
-            }
-
-            return arguments;
-        }
-
-        private static string GetExpressionValue(Expression argument)
-        {
-            try
-            {
-                var compiledExpression = Expression.Lambda(argument).Compile().DynamicInvoke();
-
-                if (compiledExpression == null)
-                {
-                    return "null";
-                }
-
-                if (TypeHelper.IsFuncOrAction(compiledExpression.GetType()))
-                {
-                    var func = (Delegate) compiledExpression;
-
-                    return func.DynamicInvoke()?.ToString();
-                }
-
-                if (TypeHelper.IsIEnumerable(compiledExpression) || compiledExpression.GetType().IsArray)
-                {
-                    return string.Join(", ", (IEnumerable<object>) compiledExpression);
-                }
-
-                if (TypeHelper.IsIDictionary(compiledExpression))
-                {
-                    return string.Join(",",
-                        ((IDictionary<object, object>) compiledExpression).Select(kv => $"{kv.Key}={kv.Value}")
-                    );
-                }
-
-                return compiledExpression.ToString();
-            }
-            catch (Exception)
-            {
-                return "null";
-            }
+            StepText = $"{StepPrefix} {customStepText}";
         }
 
         internal async Task Execute()
