c7c6e0141e3e3f68743baece971149be1460dbb9
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-04-30 20:14:07 +0100

    OverriddenStepTexts

--- BDTest.Tests/StepTextTests.cs
+++ BDTest.Tests/StepTextTests.cs
@@ -34,6 +34,32 @@ namespace BDTest.Tests
         }
         
         [Test]
+        public void OverriddenStepTexts()
+        {
+            var text1 = "My first overridden steptext";
+            var text2 = "My second overridden steptext";
+            var text3 = "My third overridden steptext";
+            
+            var scenario = Given(() => Action1Custom()).WithStepText(() => text1)
+                .When(() => StepThatPrintsMyName("Tom", "Longhurst")).WithStepText(() => text2)
+                .Then(() => Action3Custom()).WithStepText(() => text3)
+                .And(() => Action3Custom()).WithStepText(() => text3)
+                .BDTest();
+            
+            BDTestReportGenerator.Generate();
+
+            Assert.That(scenario.Steps[0].StepText, Is.EqualTo("Given " + text1));
+            Assert.That(scenario.Steps[1].StepText, Is.EqualTo("When " + text2));
+            Assert.That(scenario.Steps[2].StepText, Is.EqualTo("Then " + text3));
+            Assert.That(scenario.Steps[3].StepText, Is.EqualTo("And " + text3));
+            
+            Assert.That(JsonHelper.GetDynamicJsonObject().SelectToken("$.Scenarios[0].Steps[0].StepText").ToString(), Is.EqualTo("Given " + text1));
+            Assert.That(JsonHelper.GetDynamicJsonObject().SelectToken("$.Scenarios[0].Steps[1].StepText").ToString(), Is.EqualTo("When " + text2));
+            Assert.That(JsonHelper.GetDynamicJsonObject().SelectToken("$.Scenarios[0].Steps[2].StepText").ToString(), Is.EqualTo("Then " + text3));
+            Assert.That(JsonHelper.GetDynamicJsonObject().SelectToken("$.Scenarios[0].Steps[3].StepText").ToString(), Is.EqualTo("And " + text3));
+        }
+        
+        [Test]
         public void DefaultMethodNameStepTexts()
         {
             var scenario = Given(() => Step1WithoutAStepTextAttribute())
