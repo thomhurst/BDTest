4d743e511bc8a542d9306ce666910705c826e399
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-06-23 19:47:37 +0100

    SemaphoreSlim to lock async methods

---
+++ BDTest.ReportGenerator.RazorServer/Extensions/SemaphoreSlimExtension.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading.Tasks;
+
+namespace BDTest.ReportGenerator.RazorServer.Extensions
+{
+    public static class SemaphoreSlimExtension
+    {
+        public static async Task RunAsync(this System.Threading.SemaphoreSlim semaphore, Func<Task> action,
+            System.Threading.CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            try
+            {
+                await semaphore.WaitAsync(cancellationToken);
+                await action();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public static async Task<T> RunAndReturnAsync<T>(this System.Threading.SemaphoreSlim semaphore,
+            Func<Task<T>> action, System.Threading.CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            try
+            {
+                await semaphore.WaitAsync(cancellationToken);
+                return await action();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
--- BDTest.ReportGenerator.RazorServer/Implementations/DataController.cs
+++ BDTest.ReportGenerator.RazorServer/Implementations/DataController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using BDTest.Maps;
 using BDTest.ReportGenerator.RazorServer.Extensions;
@@ -15,6 +16,7 @@ namespace BDTest.ReportGenerator.RazorServer.Implementations
     {
         private readonly IMemoryCacheBdTestDataStore _memoryCacheBdTestDataStore;
         private readonly IBDTestDataStore _customDatastore;
+        private readonly SemaphoreSlim _methodLock = new SemaphoreSlim(1, 1);
         
         public DataController(IMemoryCacheBdTestDataStore memoryCacheBdTestDataStore, IServiceProvider serviceProvider)
         {
@@ -25,63 +27,72 @@ namespace BDTest.ReportGenerator.RazorServer.Implementations
         [MethodImpl(MethodImplOptions.Synchronized)]
         public async Task<BDTestOutputModel> GetData(string id)
         {
-            // Check if it's already in-memory
-            var model = await _memoryCacheBdTestDataStore.GetTestData(id);
-
-            if (model == null && _customDatastore != null)
+            return await _methodLock.RunAndReturnAsync(async () =>
             {
-                // Search the backup persistent storage
-                model = await _customDatastore.GetTestData(id);
-            }
+                // Check if it's already in-memory
+                var model = await _memoryCacheBdTestDataStore.GetTestData(id);
 
-            if (model == null)
-            {
-                return null;
-            }
-            
-            // Re-cache it to extend the time
-            await _memoryCacheBdTestDataStore.StoreTestData(id, model);
+                if (model == null && _customDatastore != null)
+                {
+                    // Search the backup persistent storage
+                    model = await _customDatastore.GetTestData(id);
+                }
 
-            return model;
+                if (model == null)
+                {
+                    return null;
+                }
 
+                // Re-cache it to extend the time
+                await _memoryCacheBdTestDataStore.StoreTestData(id, model);
+
+                return model;
+            }, CancellationToken.None);
         }
         
         [MethodImpl(MethodImplOptions.Synchronized)]
         public async Task<IEnumerable<TestRunOverview>> GetRunsBetweenTimes(DateTime start, DateTime end)
         {
-            // Check if it's already in-memory
-            var model = await _memoryCacheBdTestDataStore.GetTestRunRecordsBetweenDateTimes(start, end) ?? Array.Empty<TestRunOverview>();
-
-            if (!model.Any() && _customDatastore != null)
+            return await _methodLock.RunAndReturnAsync(async () =>
             {
-                // Search the backup persistent storage
-                model = await _customDatastore.GetTestRunRecordsBetweenDateTimes(start, end);
-                
-                foreach (var testRunRecord in model ?? Array.Empty<TestRunOverview>())
-                {
-                    await _memoryCacheBdTestDataStore.StoreTestRunRecord(testRunRecord);   
-                }
-            }
+                // Check if it's already in-memory
+                var model = await _memoryCacheBdTestDataStore.GetTestRunRecordsBetweenDateTimes(start, end) ??
+                            Array.Empty<TestRunOverview>();
 
-            return model;
+                if (!model.Any() && _customDatastore != null)
+                {
+                    // Search the backup persistent storage
+                    model = await _customDatastore.GetTestRunRecordsBetweenDateTimes(start, end);
+
+                    foreach (var testRunRecord in model ?? Array.Empty<TestRunOverview>())
+                    {
+                        await _memoryCacheBdTestDataStore.StoreTestRunRecord(testRunRecord);
+                    }
+                }
+
+                return model;
+            }, CancellationToken.None);
         }
 
         [MethodImpl(MethodImplOptions.Synchronized)]
         public async Task StoreData(BDTestOutputModel bdTestOutputModel, string id)
         {
-            if (await _memoryCacheBdTestDataStore.GetTestData(id) == null)
+            await _methodLock.RunAsync(async () =>
             {
-                // Save to in-memory cache for 8 hours for quick fetching
-                await _memoryCacheBdTestDataStore.StoreTestData(id, bdTestOutputModel);
-                await _memoryCacheBdTestDataStore.StoreTestRunRecord(bdTestOutputModel.GetOverview());
-                
-                if (_customDatastore != null)
+                if (await _memoryCacheBdTestDataStore.GetTestData(id) == null)
                 {
-                    // Save to persistent storage if it's configured!
-                    await _customDatastore.StoreTestData(id, bdTestOutputModel);
-                    await _customDatastore.StoreTestRunRecord(bdTestOutputModel.GetOverview());
+                    // Save to in-memory cache for 8 hours for quick fetching
+                    await _memoryCacheBdTestDataStore.StoreTestData(id, bdTestOutputModel);
+                    await _memoryCacheBdTestDataStore.StoreTestRunRecord(bdTestOutputModel.GetOverview());
+
+                    if (_customDatastore != null)
+                    {
+                        // Save to persistent storage if it's configured!
+                        await _customDatastore.StoreTestData(id, bdTestOutputModel);
+                        await _customDatastore.StoreTestRunRecord(bdTestOutputModel.GetOverview());
+                    }
                 }
-            }
+            }, CancellationToken.None);
         }
     }
 }
