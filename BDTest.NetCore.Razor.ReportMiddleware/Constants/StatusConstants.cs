0aec15c068cfc2721ab935b9dcb3cb9c3c9ddc39
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-06-26 13:58:52 +0100

    Click the summary tiles

---
+++ BDTest.NetCore.Razor.ReportMiddleware/Constants/StatusConstants.cs
@@ -0,0 +1,12 @@
+namespace BDTest.NetCore.Razor.ReportMiddleware.Constants
+{
+    public class StatusConstants
+    {
+        public const string All = "all";
+        public const string Success = "success";
+        public const string Failed = "failed";
+        public const string Skipped = "skipped";
+        public const string Inconclusive = "inconclusive";
+        public const string NotImplemented = "notImplemented";
+    }
+}
