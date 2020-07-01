ce94698053f4a092dc8723c2477abd05e9c2ad77
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-07-01 14:58:00 +0100

    Better uri handling

--- BDTest.NetCore.Razor.ReportMiddleware/Models/CustomLinkData.cs
+++ BDTest.NetCore.Razor.ReportMiddleware/Models/CustomLinkData.cs
@@ -4,13 +4,32 @@ namespace BDTest.NetCore.Razor.ReportMiddleware.Models
 {
     public class CustomLinkData
     {
+        private Uri _url;
+
         public CustomLinkData(string text, Uri url)
         {
             Text = text;
-            Url = url;
+            _url = url;
         }
 
-        public Uri Url { get; }
         public string Text { get; }
+
+        public string Url
+        {
+            get
+            {
+                if (_url == null)
+                {
+                    return "#";
+                }
+
+                if (_url.IsAbsoluteUri)
+                {
+                    return _url.AbsoluteUri;
+                }
+
+                return _url.ToString();
+            }
+        }
     }
 }
