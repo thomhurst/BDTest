d7be6e17e8b7520105b240a072a7ff199a4d15a6
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-07-02 16:41:11 +0100

    Hide dividers when filtering

--- BDTest.NetCore.Razor.ReportMiddleware/Constants/StatusConstants.cs
+++ BDTest.NetCore.Razor.ReportMiddleware/Constants/StatusConstants.cs
@@ -3,10 +3,5 @@ namespace BDTest.NetCore.Razor.ReportMiddleware.Constants
     public class StatusConstants
     {
         public const string All = "all";
-        public const string Success = "success";
-        public const string Failed = "failed";
-        public const string Skipped = "skipped";
-        public const string Inconclusive = "inconclusive";
-        public const string NotImplemented = "notImplemented";
     }
 }
