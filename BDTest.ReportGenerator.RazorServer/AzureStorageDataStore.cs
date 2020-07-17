48440eb654f33113215aa845dac339493da953bb
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-07-17 10:38:51 +0100

    Fix filtering on multiple fields at the same time

---
+++ BDTest.ReportGenerator.RazorServer/AzureStorageDataStore.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using BDTest.Maps;
+using BDTest.NetCore.Razor.ReportMiddleware.Interfaces;
+using BDTest.NetCore.Razor.ReportMiddleware.Models;
+using Newtonsoft.Json;
+
+namespace BDTest.ReportGenerator.RazorServer
+{
+    public class AzureStorageDataStore : IBDTestDataStore
+    {
+        private readonly BlobContainerClient _testRunSummariesContainer;
+        private readonly BlobContainerClient _testDataContainer;
+        private readonly JsonSerializer _jsonSerializer = JsonSerializer.Create();
+
+        public AzureStorageDataStore()
+        {
+            var connectionString = "";
+            var blobClient = new BlobServiceClient(connectionString);
+            
+            _testRunSummariesContainer = blobClient.GetBlobContainerClient("test-runs-summaries");
+            _testRunSummariesContainer.CreateIfNotExists();
+            
+            _testDataContainer = blobClient.GetBlobContainerClient("test-data");
+            _testDataContainer.CreateIfNotExists();
+        }
+        
+        public async Task<BDTestOutputModel> GetTestData(string id)
+        {
+            try
+            {
+                var blobData = await _testDataContainer.GetBlockBlobClient(id).DownloadAsync();
+                return _jsonSerializer.Deserialize<BDTestOutputModel>(new JsonTextReader(new StreamReader(blobData.Value.Content)));
+            }
+            catch (RequestFailedException e) when(e.Status == 404)
+            {
+                return null;
+            }
+        }
+
+        public async Task<TestRunSummary[]> GetAllTestRunRecords()
+        {
+            var testRunSummaries = new List<TestRunSummary>();
+            
+            await foreach (var blobItem in _testRunSummariesContainer.GetBlobsAsync())
+            {
+                var downloadAsync = await _testRunSummariesContainer.GetBlockBlobClient(blobItem.Name).DownloadAsync();
+                var testRunSummary = _jsonSerializer.Deserialize<TestRunSummary>(new JsonTextReader(new StreamReader(downloadAsync.Value.Content)));
+
+                if (blobItem.Properties.CreatedOn < DateTimeOffset.UtcNow - TimeSpan.FromDays(90))
+                {
+                    await DeleteBlobItem(blobItem);
+                }
+                else
+                {
+                    testRunSummaries.Add(testRunSummary);
+                }
+            }
+
+            return testRunSummaries.ToArray();
+        }
+
+        private async Task DeleteBlobItem(BlobItem blobItem)
+        {
+            await Task.WhenAll(
+                _testRunSummariesContainer.DeleteBlobIfExistsAsync(blobItem.Name),
+                _testDataContainer.DeleteBlobIfExistsAsync(blobItem.Name)
+                );
+        }
+
+        public Task StoreTestData(string id, BDTestOutputModel data)
+        {
+            return _testDataContainer.GetBlockBlobClient(id).UploadAsync(data.AsStream());
+        }
+
+        public Task StoreTestRunRecord(TestRunSummary testRunSummary)
+        {
+            return _testRunSummariesContainer.GetBlockBlobClient(testRunSummary.RecordId).UploadAsync(testRunSummary.AsStream());
+        }
+    }
+}
---
+++ BDTest.ReportGenerator.RazorServer/Extensions.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using Newtonsoft.Json;
+
+namespace BDTest.ReportGenerator.RazorServer
+{
+    public static class Extensions
+    {
+        public static Stream AsStream<T>(this T t)
+        {
+            return JsonConvert.SerializeObject(t).AsStream();
+        }
+        
+        public static Stream AsStream(this string value)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(value);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
