03ca14fe21133ed2c4d1641aa3bb4e4748020613
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2020-11-03 16:02:04 +0000

    BDTest.MSTest

---
+++ BDTest.MSTest/MSTestBDTestBase.cs
@@ -0,0 +1,17 @@
+﻿using BDTest.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BDTest.MSTest
+{
+    [TestClass]
+    public abstract class MSTestBDTestBase<TContext> : AbstractContextBDTestBase<TContext>
+        where TContext : class, new()
+    {
+        [TestCleanup]
+        public void TearDown()
+        {
+            RemoveContext();
+            base.MarkTestAsComplete();
+        }
+    }
+}
