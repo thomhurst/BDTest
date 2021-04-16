c427968699f1d7fece88ec1ff249ca0a72607f8c
tom.longhurst <tom.longhurst@asos.com>
2021-04-16 12:55:45 +0100

    Refactor TestDetails.cs into helpers

---
+++ BDTest/Helpers/StoryTextHelper.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using BDTest.Attributes;
+using BDTest.Test;
+
+namespace BDTest.Helpers
+{
+    public static class StoryTextHelper
+    {
+        public static StoryText GetStoryText(BDTestBase bdTestBase)
+        {
+            var classStoryAttribute =
+                FindStoryAttribute(bdTestBase);
+
+            if (classStoryAttribute == null)
+            {
+                return null;
+            }
+
+            return new StoryText(classStoryAttribute.GetStoryText());
+        }
+
+        private static StoryAttribute FindStoryAttribute(BDTestBase bdTestBase)
+        {
+            return bdTestBase.GetType().GetCustomAttribute(typeof(StoryAttribute)) as StoryAttribute;
+        }
+    }
+}
