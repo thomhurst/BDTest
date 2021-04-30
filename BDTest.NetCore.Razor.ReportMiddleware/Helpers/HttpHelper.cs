422317973316085278554754fcc910925319b536
tom.longhurst <tom.longhurst@asos.com>
2021-04-30 11:44:40 +0100

    Pagination on TestRunList.cshtml

--- BDTest.NetCore.Razor.ReportMiddleware/Helpers/HttpHelper.cs
+++ BDTest.NetCore.Razor.ReportMiddleware/Helpers/HttpHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using BDTest.NetCore.Razor.ReportMiddleware.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 
@@ -6,7 +7,7 @@ namespace BDTest.NetCore.Razor.ReportMiddleware.Helpers
 {
     public static class HttpHelper
     {
-        public static string GetBaseUrl(HttpContext httpContext)
+        public static string GetBaseUrl(this HttpContext httpContext)
         {
             var currentUrl = new Uri(httpContext.Request.GetEncodedUrl());
             var baseUrl = currentUrl.Scheme + Uri.SchemeDelimiter + currentUrl.Host;
@@ -16,6 +17,32 @@ namespace BDTest.NetCore.Razor.ReportMiddleware.Helpers
             }
 
             return baseUrl;
+        }
+
+        public static string GetCurrentPageNumber(this HttpContext httpContext)
+        {
+            if(!httpContext.Request.Query.TryGetValue("page", out var stringPageNumber))
+            {
+                return "1";
+            }
+
+            if (string.Equals(stringPageNumber, "all", StringComparison.OrdinalIgnoreCase))
+            {
+                return "all";
+            }
+
+            return int.TryParse(stringPageNumber, out var pageNumber) ? pageNumber.ToString() : "1";
+        }
+
+        public static string GetUrlForPageNumber(this HttpContext httpContext, int pageNumber)
+        {
+            var currentUrl = GetCurrentUrl(httpContext);
+            return currentUrl.WithQueryParameter("page", pageNumber.ToString()).ToString();
+        }
+
+        public static Uri GetCurrentUrl(this HttpContext httpContext)
+        {
+            return new (httpContext.Request.GetEncodedUrl());
         }
     }
 }
