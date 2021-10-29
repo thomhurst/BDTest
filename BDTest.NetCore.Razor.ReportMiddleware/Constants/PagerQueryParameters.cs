fd25f7c2f34587ec8a386891b6cab010a5abcae8
Tom Longhurst <tom.longhurst@asos.com>
2021-10-29 16:31:31 +0100

    Quick namespace tidy

--- BDTest.NetCore.Razor.ReportMiddleware/Constants/PagerQueryParameters.cs
+++ BDTest.NetCore.Razor.ReportMiddleware/Constants/PagerQueryParameters.cs
@@ -5,13 +5,4 @@ namespace BDTest.NetCore.Razor.ReportMiddleware.Constants
         public const string Page = "page";
         public const string All = "all";
     }
-
-    public class PaginationInformation
-    {
-        public int CurrentPage { get; set; }
-        public int PageSize { get; set; }
-        public int TotalPages { get; set; }
-        public int StartPage { get; set; }
-        public int EndPage { get; set; }
-    }
 }
