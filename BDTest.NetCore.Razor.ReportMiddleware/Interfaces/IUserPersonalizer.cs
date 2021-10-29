404a28d3a6c5c232fe1abc772430f3dae8678736
Tom Longhurst <tom.longhurst@asos.com>
2021-10-29 23:08:46 +0100

    Some sidebar work

---
+++ BDTest.NetCore.Razor.ReportMiddleware/Interfaces/IUserPersonalizer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace BDTest.NetCore.Razor.ReportMiddleware.Interfaces
+{
+    public interface IUserPersonalizer
+    {
+        Uri? GetProfilePictureUri(HttpContext httpContext);
+        string? GetNameOfUser(HttpContext httpContext);
+    }
+}
