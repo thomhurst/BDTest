8df6e6d4f6ae4247ac8a10d4ae05d05ab7ceb2e2
Tom Longhurst <tom.longhurst@asos.com>
2021-10-30 13:18:52 +0100

    Fixed tooltips

---
+++ BDTest.NetCore.Razor.ReportMiddleware/Models/ViewModels/TestPageHeaderHeroViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BDTest.NetCore.Razor.ReportMiddleware.Models.ViewModels
+{
+    public class TestPageHeaderHeroViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ReportId { get; set; }
+    }
+}
