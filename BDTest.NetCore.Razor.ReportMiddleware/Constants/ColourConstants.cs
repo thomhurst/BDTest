7122f3ebe32d9aa7349147726821491aeb111eff
Tom Longhurst <tom.longhurst@asos.com>
2021-11-02 11:24:22 +0000

    Upgraded chart library

---
+++ BDTest.NetCore.Razor.ReportMiddleware/Constants/ColourConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace BDTest.NetCore.Razor.ReportMiddleware.Constants
+{
+    public class ColourConstants
+    {
+        public static readonly string PrimaryHex = "#2c3e50";
+        public static readonly string SuccessHex = "#18bc9c";
+        public static readonly string DangerHex = "#e74c3c";
+        public static readonly string WarningHex = "#f39d12";
+    }
+}
