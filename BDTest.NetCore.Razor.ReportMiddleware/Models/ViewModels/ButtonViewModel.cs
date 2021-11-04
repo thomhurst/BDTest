f1fd93434670dcbe5da7a8eee04fe98a87334aac
Tom Longhurst <tom.longhurst@asos.com>
2021-11-04 11:36:22 +0000

    Scenario status

---
+++ BDTest.NetCore.Razor.ReportMiddleware/Models/ViewModels/ButtonViewModel.cs
@@ -0,0 +1,10 @@
+using BDTest.Test;
+
+namespace BDTest.NetCore.Razor.ReportMiddleware.Models.ViewModels
+{
+    public class ButtonViewModel
+    {
+        public string Text { get; set; }
+        public Status Status { get; set; }
+    }
+}
--- BDTest.NetCore.Razor.ReportMiddleware/Models/ViewModels/TestPageHeaderHeroViewModel.cs
+++ BDTest.NetCore.Razor.ReportMiddleware/Models/ViewModels/TestPageHeaderHeroViewModel.cs
@@ -4,5 +4,6 @@
     {
         public string Title { get; set; }
         public string Description { get; set; }
+        public ButtonViewModel ButtonViewModel { get; set; }
     }
 }
