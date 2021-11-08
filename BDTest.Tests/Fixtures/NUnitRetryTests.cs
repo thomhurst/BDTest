71db259fc68fe997f74ce6e55b2f76609e4c8ed9
Tom Longhurst <tom.longhurst@asos.com>
2021-11-08 11:17:04 +0000

    RetryTests

---
+++ BDTest.Tests/Fixtures/NUnitRetryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BDTest.Attributes;
+using BDTest.NUnit;
+using NUnit.Framework;
+
+namespace BDTest.Tests.Fixtures
+{
+    public class NUnitRetryTests : NUnitBDTestBase<MyTestContext>
+    {
+        private int _testRetryCount = 0;
+        [Test, ScenarioText("RetryOnScenarioTests"), BDTestRetry(3)]
+        public void Test()
+        {
+            Given(() => Console.WriteLine("I run a test with a retry attribute"))
+                .And(() => IncrementCount(ref _testRetryCount))
+                .When(() => ThrowIfLessThan(_testRetryCount))
+                .Then(() => Console.WriteLine($"TestRetryCount: {_testRetryCount}"))
+                .BDTest();
+        }
+        
+        private void IncrementCount(ref int i)
+        {
+            i++;
+        }
+        
+        private void ThrowIfLessThan(int i)
+        {
+            Assert.That(i, Is.GreaterThanOrEqualTo(3));
+        }
+    }
+}
