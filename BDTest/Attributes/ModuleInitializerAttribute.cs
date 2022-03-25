d0aedfda23ddfce4a0de15b2973df7ea83f783af
Tom Longhurst <thomhurst@gmail.com>
2022-03-25 17:01:12 +0000

    ModuleInitializerAttribute

---
+++ BDTest/Attributes/ModuleInitializerAttribute.cs
@@ -0,0 +1,5 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public sealed class ModuleInitializerAttribute : Attribute { }
--- BDTest/BDTestServiceProvider.cs
+++ BDTest/BDTestServiceProvider.cs
@@ -34,9 +34,3 @@ namespace BDTest
         }
     }
 }
-
-namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public sealed class ModuleInitializerAttribute : Attribute { }
-}
