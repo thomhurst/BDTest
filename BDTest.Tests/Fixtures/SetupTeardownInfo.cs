0c96e53b1e21b0f2746dfc8cbbc6bf08eee274ff
Tom Longhurst <thomhurst@gmail.com>
2022-03-25 17:05:16 +0000

    Two tests

--- BDTest.Tests/Fixtures/SetupTeardownInfo.cs
+++ BDTest.Tests/Fixtures/SetupTeardownInfo.cs
@@ -34,7 +34,21 @@ public class SetupTeardownInfo : NUnitBDTestBase<MyTestContext>
             .BDTest();
             
         Assert.That(_scenario.TestStartupInformation.Trim(), Is.EqualTo("Some startup info!"));
+        Assert.That(_scenario.Steps[0].Output, Is.EqualTo("run a test"));
+        Assert.That(_scenario.Steps[1].Output, Is.EqualTo("the results should have set up and tear down information added"));
             
         Assert.That(JsonHelper.GetTestDynamicJsonObject().SelectToken("$.Scenarios[0].TestStartupInformation").ToString().Trim(), Is.EqualTo("Some startup info!"));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        _scenario = When(() => Console.WriteLine("run a test2"))
+            .Then(() => Console.WriteLine("the results should have set up and tear down information added2"))
+            .BDTest();
+            
+        Assert.That(_scenario.TestStartupInformation.Trim(), Is.EqualTo("Some startup info!"));
+        Assert.That(_scenario.Steps[0].Output, Is.EqualTo("run a test2"));
+        Assert.That(_scenario.Steps[1].Output, Is.EqualTo("the results should have set up and tear down information added2"));
     }
 }
