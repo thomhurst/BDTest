d7154197caae2e53fd6a63557039785c4f80adbf
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-10-28 18:40:38 +0100

    Fix tests predicate

--- BDTest.Pipeline/Modules/RunUnitTestsModule.cs
+++ BDTest.Pipeline/Modules/RunUnitTestsModule.cs
@@ -17,7 +17,7 @@ public class RunUnitTestsModule : Module<List<DotNetTestResult>>
         foreach (var unitTestProjectFile in context
                      .Git().RootDirectory!
                      .GetFiles(file => file.Path.EndsWith(".csproj", StringComparison.OrdinalIgnoreCase)
-                                       && file.Path.Contains("UnitTests", StringComparison.OrdinalIgnoreCase)))
+                                       && file.Path.Contains("Tests", StringComparison.OrdinalIgnoreCase)))
         {
             results.Add(await context.DotNet().Test(new DotNetTestOptions
             {
